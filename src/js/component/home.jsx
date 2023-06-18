8315ce6b859ac662820d9bb53cabe37628c8feb9
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-06-18 23:09:24 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState(["laundry", "gym", "work"])
+	const [task, setTask] = useState({})
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/stewartbenson")
+		.then((res) => {
+			return res.json()
+		})
+		.then((data) =>{
+			setTodos(data)
+		})
+	},[])
+	
+	useEffect(()=>{
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/stewartbenson", {
+			  method: "PUT",
+			  body: JSON.stringify(todos),
+			  headers: { "Content-type": "application/json" },
+			})
+			  .then((res) => {
+				return res.json();
+			  })
+			  .then((res) => console.log(res))
+			  .catch((err) => {
+				console.log(err);
+			  });
+		  },[todos]);
+
+
+	const addTodo=() =>{
+		setTodos([...todos,task])
+	}
+	const removeTodo=(index) =>{
+		setTodos((todos) => {
+			todos.filter((item, i) => i !==index)
+		})
+	}
+	const inputChange=(e) =>{
+		setTask({label: e.target.value,done:false})
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input type="text" onChange={inputChange} />
+			<button onClick={addTodo}>Add Todo</button>
+			<h1>Todos</h1>
+			<ul>
+			{todos.length ? todos.map((item, index) => {
+				return(
+					<li key={index}>{item.label}
+					<button onClick={removeTodo(index)}>x</button></li>
+				)
+			}):null}
+			</ul>
 		</div>
 	);
 };
